c8bef6a563d1933cdbe9b0947fe1bf9e63c63fa6
kishan bhakta <kishan.d.bhakta@gmail.com>
2015-06-11 00:05:58 -0400

    got all the test written for Math functions and reversedString, but cannot get cipher, decipher, and rot13 to work. The Math functions are easy to input expects().to.equal() because the numbers can be easily equated, but the others are not so easy to since I can not test them. So, I have left those inputs as empty strings.

---
+++ test/test.bar.js
@@ -0,0 +1,61 @@
+describe("Bar", function() {
+var bar;
+
+beforeEach(function() {
+  bar = new Bar();
+});
+
+
+describe('squared', function() {
+
+  it('should square a number', function() {
+    expect(bar.squared(2)).to.equal(4);
+  });
+
+});
+
+describe('cubed', function() {
+  it('should cube a number', function() {
+    expect(bar.cubed(3)).to.equal(27);
+  });
+
+});
+
+
+
+describe('exponentOf', function() {
+  it('should return a number raised to given exponent', function() {
+    var square = bar.exponentOf(2);
+    expect(square(2)).to.equal(4);
+
+  });
+});
+
+
+describe('reverseString', function() {
+  it('should reverse a string', function() {
+    expect(bar.reverseString('BAUCE')).to.equal('ECUAB');
+  });
+});
+
+
+describe('cipher', function() {
+  it('should cipher a string', function() {
+    expect(bar.cipher('')).to.equal('');
+  });
+});
+
+describe('decipher', function() {
+  it('should decipher a string', function() {
+    expect(bar.decipher('')).to.equal('');
+  });
+});
+
+
+describe('rot13', function() {
+  it('should turn a sentence into a character code string', function() {
+    expect(bar.rot13('')).to.equal('');
+  });
+});
+
+});
